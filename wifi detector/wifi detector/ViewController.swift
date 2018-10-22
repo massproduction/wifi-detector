08fe43d87040da8980c4f7b68320efc023d0865c
Panu Klemola <panu.klemola@solinor.fi>
2018-10-22 09:46:54 +0300

    Added default xcode files

---
+++ wifi detector/wifi detector/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  wifi detector
+//
+//  Created by Panu Klemola on 22/10/2018.
+//  Copyright © 2018 Panu Klemola. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
