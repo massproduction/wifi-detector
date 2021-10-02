449745aa080788d6441c47117ac89a53601f9c6a
Panu Klemola <panu.klemola@solinor.fi>
2021-10-02 09:27:46 +0300

    Trying to get notifications on wifi change

--- wifi detector/wifi detector/AppDelegate.swift
+++ wifi detector/wifi detector/AppDelegate.swift
@@ -6,16 +6,40 @@
 import Cocoa
 import CoreWLAN
 import AppKit
+import IOKit.pwr_mgt
+import SystemConfiguration
+
+func wifiChanged() {
+    print("nyt vaihtui")
+}
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
     
     var SSID = ""
-
+    var noSleepAssertionID: IOPMAssertionID = 0
+    var noSleepReturn: IOReturn? // Could probably be replaced by a boolean value, for example 'isBlockingSleep', just make sure 'IOPMAssertionRelease' do
     let statusItem = NSStatusBar.system.statusItem(withLength:NSStatusItem.squareLength)
+    
+    func getWifiSSID() -> String {
+        let interface = CWWiFiClient.shared().interface()
+        return interface?.ssid() ?? "no interface found"
+    }
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+        var context = SCDynamicStoreContext(version: 0, info: nil, retain: nil, release: nil, copyDescription: nil)
+        let dcAddress = withUnsafeMutablePointer(&context, {UnsafeMutablePointer<SCDynamicStoreContext>($0)})
+        let store = SCDynamicStoreCreate(
+            nil, "Name of your App" as CFString,
+            { ( _, _, info ) in
+                let mySelf = Unmanaged<AppDelegate>.fromOpaque(info!).takeUnretainedValue()
+                mySelf.wifiChanged()
+            }, dcAddress)!
+        SCDynamicStoreSetNotificationKeys(
+            store, [ "State:/Network/Global/IPv4" ] as CFArray, nil)
+        SCDynamicStoreSetDispatchQueue(store, DispatchQueue.main)
+        
         if let button = statusItem.button {
             button.image = NSImage(named:NSImage.Name("StatusBarButtonImage"))
         }
@@ -24,6 +48,25 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
+        enableScreenSleep()
+    }
+    
+    func disableScreenSleep(reason: String = "Unknown reason") -> Bool? {
+        guard noSleepReturn == nil else { return nil }
+        noSleepReturn = IOPMAssertionCreateWithName(kIOPMAssertionTypeNoDisplaySleep as CFString,
+                                                IOPMAssertionLevel(kIOPMAssertionLevelOn),
+                                                reason as CFString,
+                                                &noSleepAssertionID)
+        return noSleepReturn == kIOReturnSuccess
+    }
+    
+    func enableScreenSleep() -> Bool {
+        if noSleepReturn != nil {
+            _ = IOPMAssertionRelease(noSleepAssertionID) == kIOReturnSuccess
+            noSleepReturn = nil
+            return true
+        }
+        return false
     }
     
     @objc func saveWifiSSID(_ sender: Any?) {
@@ -43,16 +86,14 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         if (response == NSApplication.ModalResponse.alertFirstButtonReturn) {
             SSID = txt.stringValue
             if (SSID == getWifiSSID()) {
-                print("SSID matches the current one!")
+                print("wtf")
+                disableScreenSleep(reason: "SSID matches the current one!")
+            } else {
+                enableScreenSleep()
             }
         }
         
         print("SSID saved as " + SSID)
-    }
-    
-    func getWifiSSID() -> String {
-        let interface = CWWiFiClient.shared().interface()
-        return interface?.ssid() ?? "no interface found"
     }
     
     
